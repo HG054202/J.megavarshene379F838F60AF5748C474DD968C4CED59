430325b4c4cd7efdde02cbd982d0a8a8e5d830f3
HG054202 <144470073+HG054202@users.noreply.github.com>
2023-09-22 15:01:43 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,19 +1,13 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
+  return indices
 
-# Create objects for Batsman and Bowler
-batsman = Batsman()
-bowler = Bowler()
-
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+result = linearSearchProduct(products, target)
+print(result)
